e7cd1832fd48c85cbb75156884450ec4a45ea52f
sandip007k <sandip@vismaad.com>
2018-05-25 09:35:24 +0530

    Initial Commit

---
+++ GitProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitProject
+//
+//  Created by Joga Singh on 25/05/18.
+//  Copyright © 2018 Arethos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
